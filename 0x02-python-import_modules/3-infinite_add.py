cb49a7f36ab975aabbecec364be8dc4f0c38a38f
iamthatnikah <Kobbyaryeetey@hotmail.com>
2023-08-14 07:41:12 -0700

    Updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+
+
+if __name__ == '__main__':
+    from sys import argv
+
+    sum = 0
+    for i, arg in enumerate(argv[1:]):
+        sum += int(arg)
+
+    print('{:d}'.format(sum))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
-            print(name)
+
+    name_list = dir(hidden_4)
+    for st in name_list:
+        if not st.startswith('__'):
+            print('{:s}'.format(st))
